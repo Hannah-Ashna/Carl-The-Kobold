03c30f46bbcdc462c6d7629e3885e663fb42de00
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-02-23 00:38:25 +0000

    Created random gen script

--- DungeonGame/Assets/Scripts/RoomSpawner.cs
+++ DungeonGame/Assets/Scripts/RoomSpawner.cs
@@ -10,17 +10,47 @@ public class RoomSpawner : MonoBehaviour
     // 3 - Left Door
     // 4 - Right Door
 
-    void Update(){
-        if(openingDirection == 1){
-            // Spawn any room with a Bottom Door
-        } else if (openingDirection == 2){
-            // Spawn any room with a Top Door
-        } else if (openingDirection == 3){
-            // Spawn any room with a Left Door
-        } else if (openingDirection == 4){
-            // Spawn any room with a Right Door
-        } else {
-            print("Error");
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+    void Start(){
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("SpawnRoom", 0.1f);
+    }
+
+    void SpawnRoom(){
+        // Check if a room has already been spawned, if not then spawn room
+        // Used to prevent extra rooms from being spawned within the scene
+        if (spawned == false) {
+            if(openingDirection == 1){
+                // Spawn any room with a Bottom Door
+                rand = Random.Range(0, templates.botRooms.Length);
+                Instantiate(templates.botRooms[rand], transform.position, templates.botRooms[rand].transform.rotation);
+            } else if (openingDirection == 2){
+                // Spawn any room with a Top Door
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+            } else if (openingDirection == 3){
+                // Spawn any room with a Left Door
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+            } else if (openingDirection == 4){
+                // Spawn any room with a Right Door
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+            } else {
+                print("Error");
+            }
+
+            spawned = true;
+        }
+    }
+
+    // Prevent rooms from being spawned on top of each other
+    void onTriggerEnter2D(Collider2D collision){
+        if(collision.CompareTag("SpawnPoint") && collision.GetComponent<RoomSpawner>().spawned == true){
+            // Destroy the spawn point
+            Destroy(gameObject);
         }
     }
 }
---
+++ DungeonGame/Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+   // Arrays to categorise and store the different room prefabs
+   public GameObject[] topRooms;
+   public GameObject[] botRooms;
+   public GameObject[] rightRooms;
+   public GameObject[] leftRooms;
+}
