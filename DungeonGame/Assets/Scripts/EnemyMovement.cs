7dda0c665612c5f08d3001ce3d82adb944a12c8f
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-02-24 22:40:05 +0000

    Created Enemymovement.cs

---
+++ DungeonGame/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private NavMeshAgent nav;
+    private Transform player;
+
+    void Start(){
+        nav = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update(){
+        nav.SetDestination(player.position);
+    }
+}
