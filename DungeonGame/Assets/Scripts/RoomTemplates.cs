d8339d882ae3648075195d43ffce6cfb95383389
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-02-24 22:05:44 +0000

    Implemented random Minion spawning position

--- DungeonGame/Assets/Scripts/RoomTemplates.cs
+++ DungeonGame/Assets/Scripts/RoomTemplates.cs
@@ -17,11 +17,26 @@ public class RoomTemplates : MonoBehaviour
    // Spawn a Boss 
    public float waitTime;
    private bool spawnedBoss;
+   private int rand;
+   private int randX;
+   private int randY;
    public GameObject Boss;
    public GameObject Minion;
+
    void Update(){
       if (waitTime <= 0 && spawnedBoss == false){
-         for (int i = 0; i < rooms.Count; i++){
+         for (int i = 1; i < rooms.Count; i++){
+
+            rand = Random.Range(1, 4);
+            Vector3 minionPos;
+
+            for (int j = 0; j < rand; j++){
+               randX = Random.Range(-5, 5);
+               randY = Random.Range(-5, 5);
+               minionPos = rooms[i].transform.position + new Vector3(randX, randY, 0);
+               Instantiate(Minion, minionPos, Quaternion.identity);
+            }
+
             if(i == rooms.Count-1){
                Instantiate(Boss, rooms[i].transform.position, Quaternion.identity);
                spawnedBoss = true;
