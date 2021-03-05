04ba4dce5e0da015261f07f79cd6f0136f906b96
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-03-05 02:25:49 +0000

    Implemented Scoreboard + Better dungeon gen

--- DungeonGame/Assets/Scripts/PlayerData.cs
+++ DungeonGame/Assets/Scripts/PlayerData.cs
@@ -1,25 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class PlayerData : MonoBehaviour
 {
 
     private int health = 100;
     private double sanity = 100;
     private float timer;
+    private TextMeshProUGUI Score;
+
+    void Start(){
+        Score = GameObject.FindGameObjectWithTag("Score").GetComponent<TextMeshProUGUI>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
 
-        if (timer > 10f){
+        if (timer > 5f && sanity > 30){
             sanity -= 0.5;
-
-            print("Sanity Level: " + sanity);
-
             timer = 0;
+
+            // Check if the sanity is below the desired level
+            if (sanity <= 30){
+                health = 0;
+            }
+
+            // Update the Game's Scoreboard
+            Score.text = "Sanity: " + sanity + "\nHealth: " + health;
         }
     }
 }
