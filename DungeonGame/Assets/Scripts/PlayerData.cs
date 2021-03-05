3dbf572034c7f99398e5b356c2a4ae4918c75ba3
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-03-05 01:47:12 +0000

    Added sanity deduction system

---
+++ DungeonGame/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+
+    private int health = 100;
+    private double sanity = 100;
+    private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 10f){
+            sanity -= 0.5;
+
+            print("Sanity Level: " + sanity);
+
+            timer = 0;
+        }
+    }
+}
