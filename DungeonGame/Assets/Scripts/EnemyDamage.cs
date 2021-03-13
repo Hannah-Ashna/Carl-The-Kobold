94f110f2f5f4039c8ffdb1ddad09a928feca711a
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-03-13 15:13:58 +0000

    Created enemy projectiles

---
+++ DungeonGame/Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    private float timeBetweenShots;
+    public float startTimeBetweenShots;
+    public GameObject projectile;
+
+    void Start (){
+        timeBetweenShots = startTimeBetweenShots;
+    }
+
+    void Update(){
+        if (timeBetweenShots <= 0){
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBetweenShots = startTimeBetweenShots;
+        }
+
+        else {
+            timeBetweenShots -= Time.deltaTime;
+        }
+    }
+}
