d67a5e6fef35cddd9e69abf0fa1a7f05a69679fb
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-03-13 16:51:29 +0000

    Implemented Player Damage

---
+++ DungeonGame/Assets/Scripts/EnemyData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyData : MonoBehaviour
+{
+    public double health;
+
+    void Update (){
+        //print(health);
+    }
+
+    public void damageEnemy(){
+        if (health > 0) {
+            health -= 5;
+        }
+
+        else {
+            DestroyImmediate(gameObject);
+        }  
+    } 
+}
---
+++ DungeonGame/Assets/Scripts/PlayerDamage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour
+{
+    private GameObject enemyObj;
+
+    void Update()
+    {
+        // User attacks enemy by pressing space
+        if (Input.GetKeyDown(KeyCode.Space)){
+            print("space");
+            attackEnemy();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        // Check if the collision is with an enemy
+        if (other.CompareTag("Enemy")){
+            try {
+                enemyObj = GameObject.FindGameObjectWithTag("Enemy");
+                attackEnemy();
+            } catch {
+                // Value is NULL as there is no nearby enemy to attack
+            }
+        }
+    }
+
+    void attackEnemy(){
+        // Damage the enemy by reducing its health
+        enemyObj.GetComponent<EnemyData>().damageEnemy();
+    }
+}
