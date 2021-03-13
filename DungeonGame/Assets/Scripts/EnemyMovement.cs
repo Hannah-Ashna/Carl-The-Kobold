352155de8de47ac764651cd6028a58c214b7f68d
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-03-13 15:07:59 +0000

    Modified Enemy Movement AI

--- DungeonGame/Assets/Scripts/EnemyMovement.cs
+++ DungeonGame/Assets/Scripts/EnemyMovement.cs
@@ -6,6 +6,8 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     public float speed;
+    public float stopDist;
+    public float retreatDist;
     private Transform player;
 
     void Start(){
@@ -13,9 +15,18 @@ public class EnemyMovement : MonoBehaviour
     }
 
     void Update(){
-        if(Vector2.Distance(transform.position, player.position) < 3.5 && Vector2.Distance(transform.position, player.position) > 3){
-            Vector3 playerPos = player.position;
-            transform.position = Vector2.MoveTowards(transform.position, playerPos, speed * Time.deltaTime);
+        if(Vector2.Distance(transform.position, player.position) < 6){
+            if(Vector2.Distance(transform.position, player.position) > stopDist){
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime); 
+            } 
+            
+            else if (Vector2.Distance(transform.position, player.position) > stopDist && Vector2.Distance(transform.position, player.position) > retreatDist){
+                transform.position = this.transform.position;
+            }
+
+            else if (Vector2.Distance(transform.position, player.position) < retreatDist) {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime); 
+            }
         }
     }
 }
